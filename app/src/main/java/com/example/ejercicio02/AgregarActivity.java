35d44384490fe47d7948064db2c187a74a561e31
pau belenguer <pau.belenguer.doria@cieep.com>
2020-10-06 10:05:43 +0200

    Agregar nota ok! Eliminar nota ok!

--- app/src/main/java/com/example/ejercicio02/AgregarActivity.java
+++ app/src/main/java/com/example/ejercicio02/AgregarActivity.java
@@ -2,13 +2,43 @@ package com.example.ejercicio02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ejercicio02.modelos.Nota;
 
 public class AgregarActivity extends AppCompatActivity {
+
+    private EditText txtTitulo, txtContenido;
+    private Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+        txtTitulo=findViewById(R.id.txtTituloAgregar);
+        txtContenido=findViewById(R.id.txtContenidoAgregar);
+        btnAgregar=findViewById(R.id.btnGuardarAgregar);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txtTitulo.getText().toString().isEmpty() && !txtContenido.getText().toString().isEmpty()){
+                    Nota nota = new Nota(txtTitulo.getText().toString(), txtContenido.getText().toString());
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable("nota",nota);
+                    Intent intent= new Intent();
+                    intent.putExtras(bundle);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }else{
+                    Toast.makeText(AgregarActivity.this,"Todos son obligatorios", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ejercicio02/MainActivity.java
+++ app/src/main/java/com/example/ejercicio02/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ejercicio02;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ejercicio02.modelos.Nota;
 
@@ -34,5 +39,86 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,AGREGAR_NOTA);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AGREGAR_NOTA && resultCode == RESULT_OK){
+            if (data != null){
+                Nota nota = data.getExtras().getParcelable("nota");
+                listaNotas.add(nota);
+                final int posicion= listaNotas.size()-1;
+
+                TextView txtTitulo = new TextView(this);
+                txtTitulo.setText(nota.getTitulo());
+                txtTitulo.setTextSize(24);
+                txtTitulo.setTextColor(Color.BLUE);
+                LinearLayout.LayoutParams paramsTXT= new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,3);
+                paramsTXT.setMargins(15,15,15,15);
+                txtTitulo.setLayoutParams(paramsTXT);
+                txtTitulo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+                Button button = new Button(this);
+                button.setText("Eliminar");
+                button.setBackgroundColor(Color.GREEN);
+                LinearLayout.LayoutParams paramsBTN= new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1);
+                paramsBTN.setMargins(15,15,15,15);
+                button.setLayoutParams(paramsBTN);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listaNotas.remove(posicion);
+                        repintaNotas();
+                    }
+                });
+                LinearLayout contenedorNota = new LinearLayout(this);
+                contenedorNota.addView(txtTitulo);
+                contenedorNota.addView(button);
+                contenedor.addView(contenedorNota);
+            }
+        }
+    }
+
+    private void repintaNotas() {
+        contenedor.removeAllViews();
+        for (int i = 0; i <listaNotas.size() ; i++) {
+            Nota nota = listaNotas.get(i);
+            final int posicion= i;
+            TextView txtTitulo = new TextView(this);
+            txtTitulo.setText(nota.getTitulo());
+            txtTitulo.setTextSize(24);
+            txtTitulo.setTextColor(Color.BLUE);
+            LinearLayout.LayoutParams paramsTXT= new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,3);
+            paramsTXT.setMargins(15,15,15,15);
+            txtTitulo.setLayoutParams(paramsTXT);
+            txtTitulo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            Button button = new Button(this);
+            button.setText("Eliminar");
+            button.setBackgroundColor(Color.GREEN);
+            LinearLayout.LayoutParams paramsBTN= new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1);
+            paramsBTN.setMargins(15,15,15,15);
+            button.setLayoutParams(paramsBTN);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listaNotas.remove(posicion);
+                    repintaNotas();
+                }
+            });
+            LinearLayout contenedorNota = new LinearLayout(this);
+            contenedorNota.addView(txtTitulo);
+            contenedorNota.addView(button);
+            contenedor.addView(contenedorNota);
+        }
     }
 }
